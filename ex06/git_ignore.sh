b84eb2e85c2808c98189f5060360c6d055cc5aad
jcarbone <jcarbone@c3r11s1.42madrid.com>
2021-06-24 14:23:39 +0200

    excluded standard files

---
+++ ex06/git_ignore.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# git-ls-files - Shows information about files in the index and the working tree
+# -io flag adds ignored and others
+# --exclude-standard adds files excluded by git
+
+git ls-files -io --exclude-standard
+
